d40b168e6282c42792679d124dd44edce74b98d2
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-20 12:54:21 -0300

    initial commit - redux

---
+++ src/about/about.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default function About(props){
+    return(
+        <div>
+            <PageHeader name='Sobre' small='nós'/>
+
+
+            <h2>Nossa história</h2>
+            <p>Lorem ipsum</p>
+            <h2>Missão e visão</h2>
+            <h2>Imprensa</h2>
+        </div>
+    )
+}
