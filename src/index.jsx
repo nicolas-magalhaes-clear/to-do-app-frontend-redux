7b94e0d1d51a44845a63d3271b5f3f2417fde044
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-22 08:57:10 -0300

    update: added middlewares to deal with promises

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
 
 import rootReducer from './main/reducers'
 
@@ -11,7 +13,7 @@ import rootReducer from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(rootReducer, devTools);
+const store = applyMiddleware(promise)(createStore)(rootReducer, devTools);
 ReactDOM.render(
     <Provider store={store}>
         <App />
--- src/todo/todoActions.js
+++ src/todo/todoActions.js
@@ -1,6 +1,29 @@
+import axios from "axios"
+
+const URL = 'http://localhost:3000/api/todos'
+
 export const changeDescription = (event) => ({
 
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 
 })
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createdAt`)
+    console.log('REQUEST OK', request)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
+export const add = (description) => {
+
+    const request = axios.post(URL, {description})
+    return {
+        type: 'TODO_ADDED',
+        payload: request
+    }
+
+}
